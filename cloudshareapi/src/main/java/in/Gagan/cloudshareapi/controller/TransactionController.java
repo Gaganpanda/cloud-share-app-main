a2c42b29c88e10a6ba67ae653ad7860ab4cdae63
Gagan Pandey <157130627+Gaganpanda@users.noreply.github.com>
2025-08-21 16:47:36 +0530

    cloudsharing-app

---
+++ cloudshareapi/src/main/java/in/Gagan/cloudshareapi/controller/TransactionController.java
@@ -0,0 +1,31 @@
+package in.Gagan.cloudshareapi.controller;
+
+import in.Gagan.cloudshareapi.document.PaymentTransaction;
+import in.Gagan.cloudshareapi.document.ProfileDocument;
+import in.Gagan.cloudshareapi.repository.PaymentTransactionRepository;
+import in.Gagan.cloudshareapi.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final PaymentTransactionRepository paymentTransactionRepository;
+    private final ProfileService profileService;
+
+    @GetMapping
+    public ResponseEntity<?> getUserTransactions() {
+        ProfileDocument currentProfile = profileService.getCurrentProfile();
+        String clerkId = currentProfile.getClerkId();
+
+        List<PaymentTransaction> transactions = paymentTransactionRepository.findByClerkIdAndStatusOrderByTransactionDateDesc(clerkId, "SUCCESS");
+        return ResponseEntity.ok(transactions);
+    }
+}
---
+++ cloudshareapi/src/main/java/in/Gagan/cloudshareapi/repository/FileMetadataRepository.java
@@ -0,0 +1,13 @@
+package in.Gagan.cloudshareapi.repository;
+
+import in.Gagan.cloudshareapi.document.FileMetadataDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface FileMetadataRepository extends MongoRepository<FileMetadataDocument, String> {
+
+    List<FileMetadataDocument> findByClerkId(String clerkId);
+
+    Long countByClerkId(String clerkId);
+}
